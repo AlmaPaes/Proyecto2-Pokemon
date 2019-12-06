cf86797a18caadaa773275e49fed0a2825d09da6
AlmaPaes <paes@ciencias.unam.mx>
2019-12-05 22:24:09 -0600

    Reorganización y adición de timeouts-beta

--- client_thread.py
+++ client_thread.py
@@ -5,32 +5,22 @@ import sys
 
 CODIGO_YES = bytearray([30])
 CODIGO_NO = bytearray([31])
+CODIGO_LOGOUT = bytearray([32])
 
-
-def main():
-   soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   host = "127.0.0.1"
-   port = 9999
-   try:
-      soc.connect((host, port))
-   except:
-      print("Connection Error")
-      sys.exit()
-   print("Bienvenido a Pokemon Go! ¿Deseas capturar un Pokémon [P], revisar el catálogo? [C] o salir [S]?")
-   message = input(" >> ")
-   if message != 'S':
-        if message == 'P':
-            soc.send(bytearray([10]))
-            mensaje = soc.recv(2)
-            idPokemon = mensaje[1]
-            print("¿Capturar al Pokemon " + str(idPokemon) + "?")
-            print("Sí [S] o No [N]")
-            message = input(" >> ")
-            
-            if message == 'S':
-                soc.send(CODIGO_YES) 
-                jugando = True
-                while jugando:
+def playPokemon(soc):
+    try:
+        soc.send(bytearray([10]))
+        mensaje = soc.recv(2)
+        idPokemon = mensaje[1]
+        print("¿Capturar al Pokemon " + str(idPokemon) + "?")
+        print("Sí [S] o No [N]")
+        message = input(" >> ")
+        
+        if message == 'S':
+            soc.send(CODIGO_YES) 
+            jugando = True
+            while jugando:
+                try:
                     mensaje = soc.recv(10)
                     #print(mensaje[0])
                     respuesta = mensaje[0]
@@ -39,8 +29,22 @@ def main():
                     
                     if respuesta == 21: #aun tienes intentos
                         print("¿Intentar captura de nuevo? Quedan " + str(mensaje[2]) + " intentos")
+                        print("Sí [S] o No [N]")
                         message = input(" >> ")
-                        soc.send(CODIGO_YES)
+                        mensaje_correcto = False
+                        while mensaje_correcto == False:
+                            if message == 'S' or message == 'N':
+                                mensaje_correcto = True
+                            else:
+                                print("Opción inválida >:(")
+                                print("¿Intentar captura de nuevo? Quedan " + str(mensaje[2]) + " intentos")
+                                print("Sí [S] o No [N]")
+                                message = input(" >> ")
+                        if message == 'S':
+                            soc.send(CODIGO_YES)
+                        else:
+                            jugando = False
+                            cerrarSesion(soc)
                     else:
                         if respuesta == 22:#capturaste al pokemon
                             print("Capturaste al pokemon")
@@ -50,10 +54,45 @@ def main():
                             jugando = False
                         #if message == 'S':
                         #    soc.send(CODIGO_YES)
-            #else:
-                print("terminando sesión")
-                        
-            print("bai")
+                    cerrarSesion(soc)
+                except socket.timeout as timeout:
+                    print("Tiempo de respuesta excedido: 10 segundos")
+                    sys.exit()
+    except socket.timeout as timeout:
+        print("Tiempo de respuesta excedido: 10 segundos")
+        sys.exit()
+    #else:
+        
+
+def cerrarSesion(soc):
+    print("Terminando conexión")
+    soc.send(CODIGO_LOGOUT)
+    
+def main():
+   soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   host = "127.0.0.1"
+   port = 9999
+   try:
+      soc.connect((host, port))
+      soc.settimeout(10)
+   except:
+      print("Connection Error")
+      sys.exit()
+   opcion_correcta = False
+   while opcion_correcta == False:
+       print("Bienvenido a Pokemon Go! ¿Deseas capturar un Pokémon [P], revisar el catálogo? [C] o salir [S]?")
+       message = input(" >> ")
+       if message == 'S' or message == 'P' or message == 'C':
+           opcion_correcta = True
+
+   if message != 'S':
+        if message == 'P':
+            playPokemon(soc)
+        else:
+            print("Mostrando catálogo")
+
+   else:
+        cerrarSesion(soc)
             #print("hola")
         #else:
         #    soc.send(bytes([11]))
--- server_thread.py
+++ server_thread.py
@@ -8,6 +8,7 @@ import random
 
 def main():
    start_server()
+   
 def start_server():
    host = "127.0.0.1"
    port = 9999 # arbitrary non-privileged port
@@ -24,6 +25,7 @@ def start_server():
    # infinite loop- do not reset for every requests
    while True:
         connection, address = soc.accept()
+        soc.settimeout(10)
         ip, port = str(address[0]), str(address[1])
         print("Connected with " + ip + ":" + port)
         try:
@@ -36,42 +38,66 @@ def clientThread(connection, ip, port, max_buffer_size = 5120):
    is_active = True
    while is_active:
       #client_input = receive_input(connection, max_buffer_size) 
-      client_input = connection.recv(max_buffer_size)
-      codigo = int.from_bytes(client_input,"big")
+      try:
+        client_input = connection.recv(max_buffer_size)
+        codigo = int.from_bytes(client_input,"big")
 
-      if codigo == 10:
-          setIdPokemon = random.randint(0,151)
-          setPokemon = [20,setIdPokemon]
-          connection.send(bytearray(setPokemon))
-          intentos = random.randint(1,5)
-          print("Jugando!")
-          print("Número de intentos: " + str(intentos))
-          intento_acertado = random.randint(1,intentos)
-          intentos_disponibles = intentos
-          intento_actual = 0
-          jugando = True
+        if codigo == 10:
+            playPokemonGo(connection)
+            is_active = False
+        if codigo == 32:
+            is_active = False
+      except socket.timeout as timeout:
+          print("Tiempo de respuesta excedido: 10 segundos")
+          sys.exit()
           
-          while jugando:
-              respuesta = connection.recv(1)
-              respuesta = int.from_bytes(respuesta,"big")
-              if respuesta == 30:
-                  if intentos_disponibles == 0: #ya no hay intentos disponibles
-                      connection.send(bytearray([23]))
-                      jugando = False
-                  else:
-                      if intento_actual != intento_acertado: #no ha capturado el pokemon
-                          fallido = [21,setIdPokemon,intentos_disponibles]
-                          intento_actual = intento_actual + 1
-                          intentos_disponibles = intentos_disponibles - 1
-                          connection.send(bytearray(fallido))
-                      else: #capturado
-                          connection.send(bytearray([22]))
-                          jugando = False
-              else:
-                  jugando = False
+   cerrarSesion(connection)
           
-          print("bai")
-          
+def playPokemonGo(connection):
+    try:
+        setIdPokemon = random.randint(0,151)
+        setPokemon = [20,setIdPokemon]
+        connection.send(bytearray(setPokemon))
+        intentos = random.randint(1,5)
+        print("Jugando!")
+        print("Número de intentos: " + str(intentos))
+        intento_acertado = random.randint(1,intentos)
+        intentos_disponibles = intentos
+        intento_actual = 1
+        jugando = True
+        
+        while jugando:
+            try:
+                respuesta = connection.recv(1)
+                respuesta = int.from_bytes(respuesta,"big")
+                if respuesta == 30:
+                    if intentos_disponibles == 0: #ya no hay intentos disponibles
+                        connection.send(bytearray([23]))
+                        jugando = False
+                    else:
+                        if intento_actual != intento_acertado: #no ha capturado el pokemon
+                            intentos_disponibles = intentos_disponibles - 1
+                            fallido = [21,setIdPokemon,intentos_disponibles]
+                            intento_actual = intento_actual + 1
+                            connection.send(bytearray(fallido))
+                        else: #capturado
+                            connection.send(bytearray([22]))
+                            jugando = False
+                else:#ya no quiere jugar
+                    jugando = False
+            except socket.timeout as timeout:
+                print("Tiempo de respuesta excedido: 10 segundos")
+                sys.exit()
+        
+        print("bai")
+    except socket.timeout as timeout:
+        print("Tiempo de respuesta excedido: 10 segundos")
+        sys.exit()
+    
+def cerrarSesion(connection):
+    connection.close()
+    
+'''         
 def receive_input(connection, max_buffer_size):
    client_input = connection.recv(max_buffer_size)
    client_input_size = sys.getsizeof(client_input)
@@ -83,5 +109,6 @@ def receive_input(connection, max_buffer_size):
 def process_input(input_str):
    print("Processing the input received from client")
    return "Hello " + str(input_str).upper()
+'''
 if __name__ == "__main__":
    main()
